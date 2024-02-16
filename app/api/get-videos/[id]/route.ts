295c32f9a600bf4734958c2a29a22d972bbcebfb
stella933 <stellarai933@gmail.com>
2024-02-16 17:45:41 +0530

    2

--- app/api/get-videos/[id]/route.ts
+++
@@ -1,9 +0,0 @@
-import { getCategoryById } from "@/lib/action/category.action";
-
-
-export async function GET({ params }: { params: { id: string } },request:Request) {
-    const id = params.id;
-    const res = await getCategoryById(id); 
-   
-    return Response.json(res);
-  }
