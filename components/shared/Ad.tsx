448fca8dc02c236104bb6d1ac9810acce3024e66
stella <160188822+stella933@users.noreply.github.com>
2024-02-16 18:57:10 +0530

    Update Ad.tsx

--- components/shared/Ad.tsx
+++ components/shared/Ad.tsx
@@ -3,8 +3,8 @@
 const Ad = () => {
     return (
         <div className="w-full md:h-28 h-60 flex flex-col md:flex-row md:items-center gap-1 justify-between mt-3">
-            <div className="md:w-[49%] w-full h-[100%] bg-slate-700">ad</div>
-            <div className="md:w-[49%] w-full h-[100%] bg-slate-700">ad</div>
+            <div className="md:w-[49%] w-full h-[100%] bg-slate-700"><iframe width="315" height="300" frameborder="0" scrolling="no" src="//tsyndicate.com/iframes2/6d26d65fa65e414f92b46217aa93fdbc.html?extid={extid}"></iframe></div>
+            <div className="md:w-[49%] w-full h-[100%] bg-slate-700"><iframe width="315" height="300" frameborder="0" scrolling="no" src="//tsyndicate.com/iframes2/6d26d65fa65e414f92b46217aa93fdbc.html?extid={extid}"></iframe></div>
         </div>
     )
 }
